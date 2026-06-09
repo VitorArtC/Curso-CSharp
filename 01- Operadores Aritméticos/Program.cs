4d8548f4bdfef8d2b2364d5b920a2bec9742066f
Vitor Arthur <129072713+VitorArtC@users.noreply.github.com>
2026-06-09 01:10:59 -0300

    commit aula-01

---
+++ 01- Operadores Aritméticos/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("Digite o numero 1: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Digite o numero 2: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("n2="+ n2);
+
+        int nSum = n1 + n2; 
+        string Result = "Sua soma é igual à: ";
+
+        Console.WriteLine(string.Concat(Result,Convert.ToString(nSum)));
+    }
+}
