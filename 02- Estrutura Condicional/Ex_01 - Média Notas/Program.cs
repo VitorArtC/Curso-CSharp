5d0fd5ccacd48ab5316f0482050e703e337a5dd9
Vitor Arthur <129072713+VitorArtC@users.noreply.github.com>
2026-06-09 15:06:58 -0300

    Estrutura Condicional

---
+++ 02- Estrutura Condicional/Ex_01 - Média Notas/Program.cs
@@ -0,0 +1,24 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-=-=-=- Console -=-=-=-");
+        Console.WriteLine("Digite o 1o número: ");
+        int n1 = Convert.ToInt32(Console.ReadLine());  
+
+        Console.WriteLine("Digite o 2o número: ");
+        int n2 = Convert.ToInt32(Console.ReadLine());
+
+        int nMed = (n1 + n2) / 2;
+
+        if (nMed >= 60)
+        {
+            Console.WriteLine("Parabéns, você passou na matéria com média: "+ nMed);
+        } else
+        {
+            Console.WriteLine("Você reprovou :( com média: "+ nMed);
+        }   
+
+        
+    }
+}
---
+++ 02- Estrutura Condicional/Ex_02 - Par ou Impar/Program.cs
@@ -0,0 +1,19 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-=-=-=-=- CONSOLE -=-=-=-=-");
+        Console.WriteLine("Digite um número: ");
+        
+        int num = Convert.ToInt32(Console.ReadLine());
+        int resto = num % 2;
+
+        if (resto == 1)
+        {
+            Console.WriteLine("O número é impar");
+        } else {
+            Console.WriteLine("O número é par");
+        }
+        
+    }
+}
