c664e2f332c76d992dd97493cd9c874c64410340
VitorArtC <vitorarthur3@gmail.com>
2026-06-12 16:52:56 -0300

    Operadores Lógicos 1.1

---
+++ 05- Operadores Lógicos/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        Console.WriteLine("-=-=-=-=-= CONSOLE -=-=-=-=-=");
+
+        bool isLogged = true;
+        bool hasAdminAccess = true;
+        string acessStatus;
+    
+        if (hasAdminAccess == true)
+        {
+            acessStatus = "Liberado";
+        } else
+        {
+            acessStatus = "Negado";
+        }
+
+        Console.WriteLine("\nInformações do Usuário:");
+        Console.WriteLine("Usuário logado: " + isLogged);
+        Console.WriteLine("Acesso ao painel de admin: " + acessStatus);
+    }
+}
